e4be8d34575579752580da770c16ddeaa11f3eee
manojrammurthy <manrox.drag@gmail.com>
2015-06-05 19:52:27 +0530

    json file added

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from tutorial.items import DmozItem
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
@@ -10,7 +10,8 @@ class DmozSpider(scrapy.Spider):
 
     def parse(self, response):
         for sel in response.xpath('//ul/li'):
-            title = sel.xpath('a/text()').extract()
-            link = sel.xpath('a/@href').extract()
-            desc = sel.xpath('text()').extract()
-            print title, link, desc
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
